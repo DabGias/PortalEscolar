837de095dbc04f586149ec3e7786185106a105d0
DabGias <gabrielfurlaneti@gmail.com>
2022-10-07 18:09:41 -0300

    update

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from subalgoritmos import *
 
-admins = []
-alunos = []
-provas = []
+if __name__ == "__main__":
+    provas = []
+    senha = "aDmin@2022"
 
-opc_acesso = int(input("""Portal 🚪
-
+    opc_acesso = int(input("""Portal 🚪
+    
 1 - Admin;
 2 - Aluno;
 3 - Sair.
 
-Escolha o acesso: """))
+Escolha o acesso: """).strip())
 
-while opc_acesso != 3:
-    while opc_acesso > 3 or opc_acesso < 0:
+    while opc_acesso > 3 or opc_acesso < 1:
         print("⚠ Opção inválida! ⚠")
         opc_acesso = int(input("""Portal 🚪
 
@@ -21,9 +20,57 @@ while opc_acesso != 3:
 2 - Aluno;
 3 - Sair.
 
-Escolha o acesso: """))
+Escolha o acesso: """).strip())
 
-    if opc_acesso == 1:
-        pass
-    if opc_acesso == 2:
-        pass
+    while opc_acesso != 3:
+        if opc_acesso == 1:
+            senha_adm = input("Digite a senha: ").strip()
+
+            while senha_adm != senha:
+                print("⚠ Senha inválida! ⚠")
+                senha_adm = input("Digite a senha: ").strip()
+
+            opc_adm = int(input("""Bem-vindo(a) ADM 🤵
+            
+1 - Criar prova;
+2 - Listar alunos e provas;
+3 - Sair.
+
+Escolha sua opção: """).strip())
+
+            while opc_adm > 3 or opc_adm < 1:
+                print("⚠ Opção inválida! ⚠")
+                opc_adm = int(input("""Bem-vindo(a) ADM 🤵
+
+1 - Criar prova;
+2 - Listar alunos e provas;
+3 - Sair.
+
+Escolha sua opção: """).strip())
+
+            while opc_adm != 3:
+                if opc_adm == 1:
+                    criar_prova(provas)
+                if opc_adm == 2:
+                    listar_tudo(provas)
+
+                opc_adm = int(input("""Bem-vindo(a) ADM 🤵
+        
+1 - Criar prova;
+2 - Listar alunos e provas;
+3 - Sair.
+
+Escolha sua opção: """).strip())
+
+        if opc_acesso == 2:
+            iniciar_prova(provas)
+
+        opc_acesso = int(input("""Portal 🚪
+    
+1 - Admin;
+2 - Aluno;
+3 - Sair.
+
+Escolha o acesso: """).strip())
+
+    print("Você saiu! 👋")
--- subalgoritmos.py
+++ subalgoritmos.py
@@ -1,9 +1,10 @@
-def cria_prova(provas: list):
+def criar_prova(provas: list):
     prova = {
         "id_prova": int(input("Digite o ID da prova: ").strip()),
         "titulo_prova": input("Qual o título da prova? ").title().strip(),
         "num_quest": int(input("Qual o número de questões? ").strip()),
-        "quests": []
+        "quests": [],
+        "completou": []
     }
 
     while prova["id_prova"] < 1:
@@ -11,7 +12,7 @@ def cria_prova(provas: list):
         prova["id_prova"] = int(input("Digite o ID da prova: ").strip())
 
     for prova_salva in provas:
-        while prova["id_prova"] == prova_salva["id_prova"]:
+        if prova["id_prova"] == prova_salva["id_prova"]:
             print("⚠ Opção inválida! Já existe uma prova com esse ID! ⚠")
             prova["id_prova"] = int(input("Digite o ID da prova: ").strip())
 
@@ -21,7 +22,7 @@ def cria_prova(provas: list):
 
     for num_quest in range(prova["num_quest"]):
         quest = {
-            "enunciado": input("Digite o enunciado: ").strip().capitalize(),
+            "enunciado": input("Digite o enunciado da questão {}: ".format(num_quest + 1)).strip().capitalize(),
             "num_alt": int(input("Qual o número de alternativas? ").strip()),
             "alts": [],
             "resp_corr": ""
@@ -32,9 +33,57 @@ def cria_prova(provas: list):
             quest["num_alt"] = int(input("Qual o número de alternativas? ").strip())
 
         for num_alt in range(quest["num_alt"]):
-            alt = input("Digite a alternativa: ".strip().capitalize())
+            alt = input("Digite a alternativa: ").strip().upper()
             quest["alts"].append(alt)
+
+        quest["resp_corr"] = input("Qual a alternativa correta? ").strip().upper()
 
         prova["quests"].append(quest)
 
     provas.append(prova)
+
+
+def listar_tudo(provas: list):
+    if len(provas) > 0:
+        for index_p, prova in enumerate(provas):
+            print("-" * 20)
+            print("{}. {}: ".format(index_p + 1, prova["titulo_prova"]))
+            for index_a, aluno in enumerate(prova["completou"]):
+                print("    {}. Nome: {} Nota: {}/{}".format(index_a + 1, aluno["nome_aluno"], aluno["nota_aluno"],
+                                                            prova["num_quest"]))
+        print("-" * 20)
+    else:
+        print("⚠ Não há provas registradas! ⚠")
+
+
+def iniciar_prova(provas: list):
+    if len(provas) > 0:
+        aluno = {
+            "nome_aluno": input("Digite seu nome: "),
+            "nota_aluno": 0
+        }
+
+        id_prova = int(input("Digite o ID da prova: "))
+
+        while id_prova < 0:
+            print("⚠ Opção inválida! ⚠")
+            id_prova = int(input("Digite o ID da prova: "))
+
+        for prova in provas:
+            if prova["id_prova"] == id_prova:
+                for quest in prova["quests"]:
+                    print("-" * 20)
+                    print(quest["enunciado"])
+                    for alt in quest["alts"]:
+                        print(alt)
+                    resp_aluno = input("Digite a alternativa: ").strip().upper()
+
+                    if quest["resp_corr"] == resp_aluno:
+                        aluno["nota_aluno"] += 1
+
+                print("-" * 20)
+                print("Nota: {}".format(aluno["nota_aluno"]))
+                print("-" * 20)
+                prova["completou"].append(aluno)
+    else:
+        print("⚠ Não há provas cadastradas! ⚠")
